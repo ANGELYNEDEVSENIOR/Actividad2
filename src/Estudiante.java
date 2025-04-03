47c104d5676d4eaa5fcbe8e163546045b4f3fa5c
Angelyne Mariana Gonzalez Pinto <angelynemarianagonzalespinto@gmail.com>
2025-04-03 00:09:27 -0500

    Realizamos extencion de clase padre a clase hija

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,5 +1,16 @@
-public class Estudiante {
-    //
+public class Estudiante extends Persona{
+    //Hacemos una extencion donde heredamos de clase padre
+    private int codigoEstudiante;
+    private float notaFinal;
+    //Realizamos el constructor 
+    public Estudiante(String nombre, String apellidos, int edad, int codigoEstudiante, float notaFinal) {
+        super(nombre, apellidos, edad);
+        // con la palabra super heredamos de la clase padre
+        this.codigoEstudiante = codigoEstudiante;
+        this.notaFinal = notaFinal;
+    }
+    
+
  
 
 }
