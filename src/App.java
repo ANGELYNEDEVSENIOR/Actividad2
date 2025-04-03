81b86790d78d5127994723426f8e1f4d082def62
Angelyne Mariana Gonzalez Pinto <angelynemarianagonzalespinto@gmail.com>
2025-04-03 00:37:22 -0500

    creamos objeto docente y instanciamos

--- src/App.java
+++ src/App.java
@@ -17,6 +17,19 @@ public class App {
     16, 311, 3.5f);
     e5.mostrarDatos();
     // Cuando agregamos la f al final indicamos que es un numero flotante
+
+    //creamos objeto docente, donde instanciamos 
+    Docente d1 = new Docente("Alicia", "Sanchez Priss", 23, 1000);
+    d1.mostrarDatos();
+    Docente d2 = new Docente("Patricia", "Wilches Guedes", 27, 1500);
+    d2.mostrarDatos();
+    Docente d3 = new Docente("Isabella", "Huertas Cañon", 20, 1450);
+    d3.mostrarDatos();
+    Docente d4 = new Docente("Victor", "Garcia Leon", 30, 2000);
+    d4.mostrarDatos();
+    Docente d5 = new Docente("Leonardo","Parra Parra", 32, 2200);
+    d5.mostrarDatos();
     }
-    
+     
+ 
 }
