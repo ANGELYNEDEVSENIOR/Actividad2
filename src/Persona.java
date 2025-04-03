b9ecb1d862abc7eda9735dbce1115710bb6a16bb
Angelyne Mariana Gonzalez Pinto <angelynemarianagonzalespinto@gmail.com>
2025-04-03 00:06:37 -0500

    Creamos metodos accesores get and set

--- src/Persona.java
+++ src/Persona.java
@@ -8,6 +8,26 @@ public class Persona {
         this.apellidos = apellidos;
         this.edad = edad;
     }
+    //Realizamos lo metodos accesores getter and setter
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     
 }
 
