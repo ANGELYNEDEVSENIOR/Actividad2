f95aec501f07354b70d5d6d094eb4ddacabd8ddc
Angelyne Mariana Gonzalez Pinto <angelynemarianagonzalespinto@gmail.com>
2025-04-03 00:05:18 -0500

    Creacion de constructor en clase padre

---
+++ src/Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+ // Ubicamos los atributos de la clase padre Persona
+    private String nombre, apellidos;
+    private int edad;
+    // Realizamos el constructor
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+    
+}
+
